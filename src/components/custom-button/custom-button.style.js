26a2b599fcf7bac4bf6343cea4908755966af1b2
Alaatohamy <alaatohamy4@gmail.com>
2019-10-24 21:52:05 +0200

    Feat: Add styled button reusable component

---
+++ src/components/custom-button/custom-button.style.js
@@ -0,0 +1,21 @@
+import styled, { css } from "styled-components";
+
+const blue = css`
+  background-color: #4285f4;
+  border: 1px solid #4285f4;
+
+  &:hover {
+    background-color: #fff;
+    color: #4285f4;
+  }
+`;
+
+export const Button = styled.button`
+  padding: 15px 25px;
+  font-size: 20px;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 5px;
+
+  ${blue}
+`;

aadf139794b120fa29549efbfce8b02e1620c7aa
Alaatohamy <alaatohamy4@gmail.com>
2019-10-24 18:02:52 +0200

    Feat: Add main header with its styling as component

---
+++ src/components/header/header.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainHeader = styled.header`
+  background-color: #fff;
+  padding: 25px;
+`;
+
+export const HeaderTitle = styled.h1`
+  margin: 0;
+  text-transform: uppercase;
+  font-size: 16px;
+  letter-spacing: 3px;
+`;
---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainHeader, HeaderTitle } from "./header.style";
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <HeaderTitle>Cafe React</HeaderTitle>
+    </MainHeader>
+  );
+};
+
+export default Header;

a369da5375cba0b003d112cc144abb9c4ed920bf
Alaatohamy <alaatohamy4@gmail.com>
2019-10-29 21:15:28 +0200

    Feat: Add functionallity of add, edit, delete menu item with firestore

--- src/api-functions/menu-list.js
+++ src/api-functions/menu-list.js
@@ -2,8 +2,8 @@ import { firestore } from "firebase-config/utils";
 
 export const addNewItem = async newItem => {
   const collectionRef = await firestore.collection("menu-items");
-  const newDoc = collectionRef.doc();
-  await newDoc.set(newItem);
+  const newItemRef = collectionRef.doc();
+  await newItemRef.set(newItem);
 };
 
 export const getAllItems = async () => {
@@ -14,4 +14,21 @@ export const getAllItems = async () => {
     ...item.data()
   }));
   return allItems;
+};
+
+export const getItemData = async id => {
+  const itemRef = await firestore.doc(`menu-items/${id}`);
+  const itemSnapshot = await itemRef.get();
+  const itemData = await itemSnapshot.data();
+  return itemData;
+};
+
+export const deleteItem = async id => {
+  const itemRef = await firestore.doc(`menu-items/${id}`);
+  await itemRef.delete();
+};
+
+export const editItem = async (id, newData) => {
+  const itemRef = await firestore.doc(`menu-items/${id}`);
+  const itemSnapshot = await itemRef.update(newData);
 };
